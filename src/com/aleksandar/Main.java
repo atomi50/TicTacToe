cb0714e2d0028e7545a334f6e09885260dcd8801
Aleksandar <atomi50@yahoo.com>
2021-03-18 12:09:47 +0100

    Test initial commit

---
+++ src/com/aleksandar/Main.java
@@ -0,0 +1,9 @@
+package com.aleksandar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TicTacToe game = new TicTacToe();
+    }
+}
---
+++ src/com/aleksandar/TicTacToe.java
@@ -0,0 +1,44 @@
+package com.aleksandar;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textField = new JLabel();
+    JButton[] buttons = new JButton[9];
+
+
+    public TicTacToe() {
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
+
+}
